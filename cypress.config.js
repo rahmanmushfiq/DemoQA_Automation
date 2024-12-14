7b5a5b475ab6cc4567e3cfd684570721e385851e
Mushfiqur Rahman <mushfiq.aiub142@hotmail.com>
2024-12-14 11:05:41 +0600

    Added mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,12 @@ module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false,
     defaultCommandTimeout: 3000,
+    reporter: 'cypress-mochawesome-reporter',
     //experimentalOriginDependencies: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      this.screenshotOnRunFailure = true;
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
