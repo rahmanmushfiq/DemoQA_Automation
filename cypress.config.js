edc6d17ea29df7d6dfe72932c07a0ea7037b3b28
Mushfiqur Rahman <18183842+rahmanmushfiq@users.noreply.github.com>
2025-03-06 14:16:25 +0600

    Made testIsolation to false on cypress.config.js in order to persist form data for validation on different "it" block

--- cypress.config.js
+++ cypress.config.js
@@ -6,12 +6,17 @@ module.exports = defineConfig({
     defaultCommandTimeout: 3000,
     experimentalMemoryManagement: true,
     numTestsKeptInMemory: 0,
+    testIsolation: false,
     reporter: 'cypress-mochawesome-reporter',
     //experimentalOriginDependencies: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       this.screenshotOnRunFailure = true;
       require('cypress-mochawesome-reporter/plugin')(on);
+      /*if (config.specPattern.includes('TestFormsPagePracticeForm.cy.js')) {
+        config.testIsolation = false;
+      }
+      return config;*/
     },
     baseUrl: "https://demoqa.com/",
   },
--- cypress/e2e/TestFormsPagePracticeForm.cy.js
+++ cypress/e2e/TestFormsPagePracticeForm.cy.js
@@ -8,18 +8,22 @@ let userData;
 before(() => {
     cy.fixture('PracticeFormPageData.json').then((data) => {
         userData = data;
-    })
+    });
 })
 
 describe("It checks the forms page functionality", function () {
 
-    it("Go to DemoQA Practice Form URL and Fillup the form and validates it", function () {
+    it("Go to DemoQA Practice Form URL and Fillup the form", function () {
         utilities.navigateTo(userData.practiceFormURL);
 
         formsPagePracticeForm.validateFormPage(userData.expectedHeader, userData.expectedFormHeader);
 
         formsPagePracticeForm.fillUpForm(userData.firstName, userData.lastName, userData.email, userData.mobile, userData.englishSubject, userData.chemistrySubject, userData.dateOfBirth, userData.currentAddress, userData.state, userData.city);
+    });
+
+    it("Validate the form data", function () {
 
         formsPagePracticeForm.validateFormData(userData.formSubmissionMessage, userData.firstName, userData.lastName, userData.email, userData.gender, userData.mobile, userData.englishSubject, userData.chemistrySubject, userData.hobbies, userData.dateOfBirth, userData.currentAddress, userData.state, userData.city, userData.filePathAndName);
     });
-})
+});
+
